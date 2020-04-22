5c102dc89045dc665820e57e1f961486c90cbe25
Wilbert Berendsen <wbsoft@xs4all.nl>
2020-04-22 17:39:26 +0200

    use a current git checkout of the RTD theme

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -113,14 +113,14 @@ todo_include_todos = False
 #
 #html_theme = 'alabaster'
 
-import sphinx_rtd_theme
+#import sphinx_rtd_theme
 
 extensions.extend([
     "sphinx_rtd_theme",
 ])
 
 html_theme = "sphinx_rtd_theme"
-
+html_theme_path = ['../..'] # we need the current git version of the theme
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
