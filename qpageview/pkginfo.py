4809c0e8f04c76cd812f13e6063108ac943418f8
Wilbert Berendsen <wbsoft@xs4all.nl>
2020-10-22 21:21:47 +0200

    ver bump to 0.6.0

--- qpageview/pkginfo.py
+++ qpageview/pkginfo.py
@@ -34,7 +34,7 @@ Version = collections.namedtuple("Version", "major minor patch")
 name = "qpageview"
 
 #: the current version
-version = Version(0, 5, 1)
+version = Version(0, 6, 0)
 version_suffix = ""
 version_string = "{}.{}.{}".format(*version) + version_suffix
 
